e6762e1066a0040167b414f1be2b3d0d4c6e41d0
Paul Bonser <pib@paulbonser.com>
2018-11-11 20:52:15 -0600

    Add writing of kubernetes secret, cli args

--- iam_roller.py
+++ iam_roller.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import logging
 
@@ -5,6 +6,7 @@ import requests
 from requests import RequestException
 
 import boto3
+from kubernetes import client, config
 
 
 def get_raw_metadata(path):
@@ -32,7 +34,61 @@ def get_metadata_field(path, field):
     return response_json[field]
 
 
-account_id = get_metadata_field('/latest/meta-data/identity-credentials/ec2/info', 'AccountId')
-role_name = get_raw_metadata('/latest/meta-data/iam/security-credentials/').split('\n')[0]
+def get_role_arn():
+    account_id = get_metadata_field('/latest/meta-data/identity-credentials/ec2/info', 'AccountId')
+    role_name = get_raw_metadata('/latest/meta-data/iam/security-credentials/').split('\n')[0]
 
-print('arn:aws:iam::{}:role/{}'.format(account_id, role_name))
+    role_arn = 'arn:aws:iam::{}:role/{}'.format(account_id, role_name)
+    return role_arn
+
+
+def assume_role(role_arn):
+    sts = boto3.client('sts')
+    res = sts.assume_role(RoleArn=role_arn, RoleSessionName='iam_roller', DurationSeconds=1800)
+    return res
+
+
+def make_creds_file(credentials):
+    creds_file = '''
+[default]
+aws_access_key_id={AccessKeyId}
+aws_secret_access_key={SecretAccessKey}
+aws_session_token={SessionToken}
+'''.format(**credentials)
+    return creds_file
+
+
+def write_secret(namespace, name, data):
+    kapi = client.CoreV1Api()
+    sec = client.V1Secret()
+    sec.metadata = client.V1ObjectMeta(name=name)
+    sec.type = 'Opaque'
+    sec.string_data = data
+
+    res = kapi.create_namespaced_secret(namespace=namespace, body=sec)
+    return res
+
+
+def run(namespace, name):
+    config.load_incluster_config()
+
+    role_arn = get_role_arn()
+    logging.info('Generating temporary credentials for role %s', role_arn)
+    creds = assume_role(role_arn)
+    creds_file = make_creds_file(creds['Credentials'])
+
+    res = write_secret(namespace, name, data={'credentials': creds_file})
+    logging.info('Response: %s', res)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Grab a rolling short-lived IAM key/secret and save it in a Kubernetes secret.')
+    parser.add_argument('--namespace', required=True,
+                        help="Namespace in which secret should be stored")
+    parser.add_argument('--name', required=True,
+                        help="Name under which the secret should be stored")
+
+    args = parser.parse_args()
+
+    run(args.namespace, args.name)
